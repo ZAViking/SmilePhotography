0578228434a269cd237d774c99f2aed14f5ce501
JustinGreen <Justin.Green@stanchionpayments.com>
2025-10-25 21:31:46 +0200

    Config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/content'],
-  content: {
-    // You can add specific sources if needed
-    sources: {
-      content: {
-        driver: 'fs',
-        base: './content'
-      }
-    }
-  }
+  devtools: { enabled: true }
+  // modules: ['@nuxt/content'],
+  // content: {
+  //   // You can add specific sources if needed
+  //   sources: {
+  //     content: {
+  //       driver: 'fs',
+  //       base: './content'
+  //     }
+  //   }
+  // }
 })
